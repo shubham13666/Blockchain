5026408d4c1fb607437c360b2b0f0e0dee253bd5
Shubham Gupta <shubham13666@gmail.com>
2018-02-22 00:19:05 +0530

    First Upload
    Block, String to Hash, JavaChain

---
+++ src/Block.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Block
+{
+    public String hash,previousHash;
+    private String data;
+    private long timeStamp;
+
+    //Block Constructor
+    public Block(String data, String previousHash)
+    {
+        this.data=data;
+        this.previousHash=previousHash;
+        this.timeStamp=new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringToHash.StringToSHA256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
+        return calculatedhash;
+    }
+
+
+}
