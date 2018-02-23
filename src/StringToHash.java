6f3239352e683053be69b82374747942d8a9c3ec
Shubham Gupta <shubham13666@gmail.com>
2018-02-23 10:24:38 +0530

    Update StringToHash.java

--- src/StringToHash.java
+++ src/StringToHash.java
@@ -1,4 +1,6 @@
 import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
 
 public class StringToHash
 {
@@ -19,11 +21,57 @@ public class StringToHash
                     hexString.append('0');
                 hexString.append(hex);
             }
-          
+
             return hexString.toString();
         }
-        catch(Exception e) {
+        catch(Exception e)
+        {
             throw new RuntimeException(e);
         }
     }
+
+    //Applies ECDSA Signature and returns the result ( as bytes ).
+
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+
+    //Verifies a String signature
+
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature)
+    {
+        try
+        {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //returns encoded string from any key.
+
+    public static String getStringFromKey(Key key)
+    {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
 }
