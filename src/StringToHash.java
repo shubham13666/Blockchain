7ffd115ae106d85eff660ea883406c7df3fd8026
ShipraliGupta123 <shipraligupta@gmail.com>
2018-02-23 03:46:06 -0800

    Update StringToHash.java

--- src/StringToHash.java
+++ src/StringToHash.java
@@ -1,6 +1,6 @@
 import java.security.MessageDigest;
 import java.security.*;
-import java.util.Base64;
+import java.util.*;
 
 public class StringToHash
 {
@@ -73,5 +73,31 @@ public class StringToHash
     {
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
+
+    //Tacks in array of transactions and returns a merkle root.
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<String>();
+        for(Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+        ArrayList<String> treeLayer = previousTreeLayer;
+        while(count > 1) {
+            treeLayer = new ArrayList<String>();
+            for(int i=1; i < previousTreeLayer.size(); i++) {
+                treeLayer.add(StringToSHA256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
+    }
+
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
 
 }
